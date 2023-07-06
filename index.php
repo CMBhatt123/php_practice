f54919d595d0780cd46c41b7644a7449b2e73f9c
CMBhatt123 <Chandra.755421@snsc.tu.edu.np>
2023-07-06 13:34:37 +0545

    added object2

--- index.php
+++ index.php
@@ -4,9 +4,11 @@ var $model; //properties
 function showModel($number){
     global $model;
     $model =$number;
-    echo "Model number is: $model";
+    echo "Model number is: $model <br>";
 }
 }
 $samsung = new Mobile;
 $samsung-> showModel('A10');
+$lg = new Mobile;
+$lg-> showModel('G5');
 ?>
